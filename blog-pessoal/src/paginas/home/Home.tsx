b104bc5bb8c131bae9012db570e68f0bc7593687
Nayara <n.carvalhosilvabr@gmail.com>
2022-10-27 14:56:50 -0300

    componente Home criado e estilizado com CSS

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src="https://i.imgur.com/dxvxbBw.gif" alt="Bob Esponja" className='img' />
+        </>
+    )
+
+}
+
+export default Home;
